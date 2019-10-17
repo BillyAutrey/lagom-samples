5dc8359c6aad803d079ea18cb9b2cea1fc4dbc35
William Autrey <william.h.autrey@us.pwc.com>
2019-10-17 11:14:21 -0400

    Implemented service spec

--- lagom-sensor-stats/lagom-sensor-stats-impl/src/test/scala/com/example/lagomsensorstats/impl/LagomSensorStatsServiceSpec.scala
+++ lagom-sensor-stats/lagom-sensor-stats-impl/src/test/scala/com/example/lagomsensorstats/impl/LagomSensorStatsServiceSpec.scala
@@ -1,5 +1,8 @@
 package com.example.lagomsensorstats.impl
 
+import java.util.UUID
+
+import akka.NotUsed
 import com.lightbend.lagom.scaladsl.server.LocalServiceLocator
 import com.lightbend.lagom.scaladsl.testkit.ServiceTest
 import org.scalatest.{AsyncWordSpec, BeforeAndAfterAll, Matchers}
@@ -14,25 +17,35 @@ class LagomSensorStatsServiceSpec extends AsyncWordSpec with Matchers with Befor
     new LagomSensorStatsApplication(ctx) with LocalServiceLocator
   }
 
+  def generateId: String = UUID.randomUUID.toString
+
   val client: LagomSensorStatsService = server.serviceClient.implement[LagomSensorStatsService]
 
   override protected def afterAll(): Unit = server.stop()
 
-//  "Lagom Sensor Stats service" should {
-//
-//    "say hello" in {
-////      client.hello("Alice").invoke().map { answer =>
-////        answer should ===("Hello, Alice!")
-////      }
-//    }
-//
-//    "allow responding with a custom message" in {
-////      for {
-////        _ <- client.useGreeting("Bob").invoke(GreetingMessage("Hi"))
-////        answer <- client.hello("Bob").invoke()
-////      } yield {
-////        answer should ===("Hi, Bob!")
-////      }
-//    }
-//  }
+  "Lagom Sensor Stats service" should {
+
+    "create a sensor" in {
+      val id = generateId
+      client.createSensor().invoke(Sensor(id)).map{ response =>
+        response shouldBe a [NotUsed]
+      }
+    }
+
+    "update and retrieve a sensor" in {
+      val id = generateId
+      val data = Math.random * 100
+
+      for {
+        create <- client.createSensor().invoke(Sensor(id))
+        update <- client.updateSensorData().invoke(SensorData(id,data.toString))
+        get <- client.getSensorData(id).invoke()
+      } yield {
+        create shouldBe a [NotUsed]
+        update shouldBe a [NotUsed]
+        get shouldBe a [SensorData]
+        get.data should ===(data.toString)
+      }
+    }
+  }
 }
